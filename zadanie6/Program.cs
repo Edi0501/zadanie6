f5204fa3065d3439f4f239e10a574cfea4c938c5
Edi0501 <edytanowakowska05@gmail.com>
2024-02-18 08:45:51 +0100

    Add project files.

---
+++ zadanie6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Dynamic;
+
+namespace zadanie6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj trzy rózne liczby");
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            int a =25;
+            int b = 63;
+            int c = 79;
+            
+            Int32.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Podaj drugą liczbę");
+            Int32.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Podaj trzecią liczbę");
+            Int32.TryParse(Console.ReadLine(), out c);
+            if (a > b && a > c)
+            {
+                Console.WriteLine("Pierwsza liczba jest największa");
+            }
+            else if (b > a && b > c) 
+            {
+                Console.WriteLine("Druga liczba jest największa");
+            }
+            else if (c > a && c > b)
+            {
+                Console.WriteLine("Trzecia  liczba jest największa");
+            }
+            else if ( a == b && a == c)
+            {
+                Console.WriteLine("Liczby maja ta samą wartość");
+            }
+            else
+            {
+                Console.WriteLine("Podałeś złe liczby");
+            }
+            
+        }
+    }
+}
